004db109c68e479ced115f04d8c977c16573fd4d
twburk <burkt95@gmail.com>
2018-12-02 23:35:18 -0500

    update

--- index.js
+++ index.js
@@ -161,6 +161,6 @@ app.get('/api/activity', function(req, res){
     });
 });
 
-app.listen(3000, function() {
-    console.log('Maryland Activities listening on port 3000!');
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening!');
 });
